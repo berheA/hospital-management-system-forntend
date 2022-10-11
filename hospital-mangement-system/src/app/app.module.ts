e29f81e422c94b3838137896005fa69d288a2f17
berheA <addberhe2@gmail.com>
2022-10-11 13:52:48 -0700

    Creating module that is a container for our components

---
+++ hospital-mangement-system/src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClient, HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ForbiddenComponent } from './components/forbidden/forbidden.component'
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from './_auth/auth.guard';
+import { AuthInterceptor } from './_auth/auth.interceptor';
+import { UserService } from './_services/user.service';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MedicalCenterDetailsComponent } from './components/medical-center-details/medical-center-details.component';
+import { UpdateMedicalCenterComponent } from './components/update-medical-center/update-medical-center.component';
+import { CreateMedicalCenterComponent } from './components/create-medical-center/create-medical-center.component';
+import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
+import { CreateAppointmentComponent } from './components/create-appointment/create-appointment.component';
+import { UpdateAppointmentComponent } from './components/update-appointment/update-appointment.component';
+import { PatientDetailsComponent } from './components/patient-details/patient-details.component';
+import { CreatePatientComponent } from './components/create-patient/create-patient.component';
+import { UpdatePatientComponent } from './components/update-patient/update-patient.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent,
+    HeaderComponent,
+    ForbiddenComponent,
+    CreateAccountComponent,
+    ChatComponent,
+    MedicalCenterDetailsComponent,
+    UpdateMedicalCenterComponent,
+    CreateMedicalCenterComponent,
+    AppointmentDetailsComponent,
+    CreateAppointmentComponent,
+    UpdateAppointmentComponent,
+    PatientDetailsComponent,
+    CreatePatientComponent,
+    UpdatePatientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+   // HttpCleintModule,
+   HttpClientModule,
+    FormsModule,
+    RouterModule
+  ],
+  providers: [
+    AuthGuard,
+    // {
+    //     provide:HTTP_INTERCEPTORS,
+    //     useClass:AuthInterceptor,
+    //     multi:true
+    // }
+    // ,
+    UserService ,
+   // RestAPIService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
